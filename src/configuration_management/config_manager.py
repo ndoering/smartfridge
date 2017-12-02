6256020cb8625ec31498be72d89083ac02e678c9
Nils D. Döring <mail@nils-doering.de>
2017-12-03 00:25:46 +0100

    Started basic configuration management.

---
+++ src/configuration_management/config_manager.py
@@ -0,0 +1,16 @@
+import configparser
+
+CONFIG_FILE = "config.ini"
+
+config = configparser.ConfigParser()
+
+def read_config():
+    config.read(CONFIG_FILE)
+
+def write_config():
+    with open(CONFIG_FILE, 'w') as configfile:
+        config.write(configfile)
+
+if __name__ == "__main__":
+    read_config()
+    print(config['SLACK']['BotName'])
