18ae7f60ba03144690dd8bdf1c710feb65323c42
jo3rn <jo3rn@users.noreply.github.com>
2017-12-15 13:37:40 +0100

    basic connection to clarifai API (upload image and get response)

---
+++ smartfridge/clarifai_connector/clarifai_connector.py
@@ -0,0 +1,15 @@
+import configparser
+
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+# TODO: call APIKey over ini file
+app = ClarifaiApp(api_key='b9a2de7676aa4fadb74a5c59ea15ecba')
+
+# initialize the trained clarifai model
+model = app.models.get('smartfridge')
+# pass filename of image to be predicted
+response = model.predict_by_filename('2017-12-06-1000.jpg')
+
+# prints the data for the defined concepts of the model
+print(response['outputs'][0]['data']['concepts'])
