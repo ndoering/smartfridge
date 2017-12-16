9a588fa49753128c01c9800941f2e657b9e5bbdd
Nils D. Döring <mail@nils-doering.de>
2017-12-16 20:19:48 +0100

    Moved test to main function

--- smartfridge/sql_connector/smart_sql_1.py
+++ smartfridge/sql_connector/smart_sql_1.py
@@ -2,39 +2,42 @@
 
 import MySQLdb
 from datetime import datetime
-#import imageio
 
-# Open database connection
-db = MySQLdb.connect("localhost","root","","smartfridge" )
 
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
+if __name__ == "__main__":
+    # Open database connection
+    db = MySQLdb.connect("localhost", "root", "", "smartfridge")
 
-#Dropping tables
-cursor.execute("DROP TABLE fridgelog")
-cursor.execute("DROP TABLE all_fruits")
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
 
-#Create tables at first
-cursor.execute("CREATE TABLE fridgelog (fid INT AUTO_INCREMENT PRIMARY KEY, capturetime TIMESTAMP, full_image LONGBLOB, manual_labeled BOOLEAN, note CHAR(20))")
+    # Dropping tables
+    cursor.execute("DROP TABLE fridgelog")
+    cursor.execute("DROP TABLE all_fruits")
 
-cursor.execute("CREATE TABLE all_fruits (afid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, fid INT, half_image LONGBLOB, class CHAR(1), confidence FLOAT, prediction FLOAT ,FOREIGN KEY (fid) REFERENCES fridgelog(fid))")
+    # Create tables at first
+    cursor.execute(
+        "CREATE TABLE fridgelog (fid INT AUTO_INCREMENT PRIMARY KEY, capturetime TIMESTAMP, full_image LONGBLOB, manual_labeled BOOLEAN, note CHAR(20))")
 
-#now = datetime.now()
-#Insert values
-#Python gibt timestamp
-stmt = 'INSERT INTO fridgelog (note) VALUES (%s)'
-data = ("hey")
-cursor.execute(stmt, data)
+    cursor.execute(
+        "CREATE TABLE all_fruits (afid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, fid INT, half_image LONGBLOB, class CHAR(1), confidence FLOAT, prediction FLOAT ,FOREIGN KEY (fid) REFERENCES fridgelog(fid))")
 
-#test1
-cursor.execute("INSERT INTO fridgelog (note) VALUES ('hey')")
+    # now = datetime.now()
+    # Insert values
+    # Python gibt timestamp
+    stmt = 'INSERT INTO fridgelog (note) VALUES (%s)'
+    data = ("hey")
+    cursor.execute(stmt, data)
 
-#test2
-cursor.execute("INSERT INTO fridgelog (note) VALUES (%s)", ("hey") )
+    # test1
+    cursor.execute("INSERT INTO fridgelog (note) VALUES ('hey')")
 
-#test3
-cursor.execute("INSERT INTO fridgelog VALUES '', (now, NULL, true"))
-cursor.execute("INSERT INTO all_fruits")
+    # test2
+    cursor.execute("INSERT INTO fridgelog (note) VALUES (%s)", ("hey"))
 
-# disconnect from server
-db.close()
+    # test3
+    cursor.execute("INSERT INTO fridgelog VALUES '', (now, NULL, TRUE"))
+    cursor.execute("INSERT INTO all_fruits")
+
+    # disconnect from server
+    db.close()
