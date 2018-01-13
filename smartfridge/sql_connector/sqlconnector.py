39649a528b66cbf6853dc93c5556f3a3529ce292
Chris <31420426+chrzby@users.noreply.github.com>
2018-01-14 00:07:06 +0100

    DB Zugriff Read Write. Ausführen und Anzeigen aller SQL Statements.
    Wichtig ist, dass man zunächst auf der msql db eine Datenbank 'smartfridge' erzeugt.
    
    Was das Einfügen von bytestreams aus Bildern und Clarifai Klassifizierungsdaten angeht müsste das am besten Jörn auf dem System testen, wo alle Python Module funktionieren.

--- smartfridge/sql_connector/sqlconnector.py
+++ smartfridge/sql_connector/sqlconnector.py
@@ -3,6 +3,7 @@ import configuration_management as conf
 from datetime import datetime
 
 #ToDo: Implement for easier encapsulation and access.
+#Easier to store results in a list of objects
 class QueryResult():
     def __init__(self):
         self.afid = None
@@ -15,6 +16,7 @@ class QueryResult():
         self.full_image = None
         self.manual_labeled = None
         self.note = None
+        self.stmt_result = None
 
 def read_file(filename):
     with open(filename, 'rb') as f:
@@ -40,6 +42,7 @@ class MySQLConnector(SQLConnector):
         self.password = password
         self.database = database
         self.cursor = None
+        self.qrl = [] #QueryResultList
 
     def connect(self):
         self.db = msqlc.connect(host=self.host, user=self.user, password=self.password, database=self.database)
@@ -54,18 +57,17 @@ class MySQLConnector(SQLConnector):
 
     def db_create_tables(self):
         # Table 'fridgelog' contains image with all fruit
-        self.cursor.execute(
-            "CREATE TABLE fridgelog (fid INT AUTO_INCREMENT PRIMARY KEY, capturetime TIMESTAMP, full_image LONGBLOB, manual_labeled BOOLEAN, note CHAR(20))")
+        self.cursor.execute("CREATE TABLE fridgelog (fid INT AUTO_INCREMENT PRIMARY KEY, capturetime TIMESTAMP, full_image LONGBLOB, manual_labeled BOOLEAN, note CHAR(20))")
 
         # Table 'fridgelog' contains image with single fruit
         # afid - AllFruitsID, fid - FridgelogID, half_image - Seperated Fruit Image, class - class of fruit, confidence - how sure is the classifier?, prediction - between 0 and 1, note - for later usage
-        self.cursor.execute(
-            "CREATE TABLE all_fruits (afid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, fid INT, half_image LONGBLOB, class CHAR(1), confidence FLOAT, prediction FLOAT, note CHAR(20), FOREIGN KEY (fid) REFERENCES fridgelog(fid))")
+        self.cursor.execute("CREATE TABLE all_fruits (afid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, fid INT, half_image LONGBLOB, class CHAR(1), confidence FLOAT, prediction FLOAT, note CHAR(20), FOREIGN KEY (fid) REFERENCES fridgelog(fid))")
 
     def reset_tables(self):
         self.cursor.execute("TRUNCATE TABLE all_fruits")
         self.cursor.execute("TRUNCATE TABLE fridgelog")
 
+    # Executing an sql statement consisting of query and filtered values. If no values are available, please call function with an empty string.
     def execute_stmt(self, query, values):
         # TODO: Proper error handling for disconnected objects
         assert self.connected == True
@@ -76,49 +78,66 @@ class MySQLConnector(SQLConnector):
                 self.cursor.execute(self.query)
             else:
                 self.values = values
-                self.cursor.execute(self.query, self.values)
-            self.stmt_result = self.cursor.fetchall()
+                self.cursor.execute(self.query % self.values)
+            #self.stmt_result = self.cursor.fetchall()
+            qr.stmt_result = self.cursor.fetchall()
 
-    def print_fridgelog(self, arg):
+    # Queries for 'fridgelog'-table, depending on ID
+    def print_fridgelog(self, mode, arg):
+        #The method uses several mode parameters for several queries
         query1 = "SELECT * FROM fridgelog"
         query2 = "SELECT * FROM fridgelog WHERE fridgelog.fid = %s"
-        if arg == "":
+        query3 = "SELECT * FROM fridgelog WHERE fridgelog.capturetime = %s"
+        if mode == 1:
             dbhdl.execute_stmt(query1,"")
-        else:
+        elif mode == 2:
             dbhdl.execute_stmt(query2, arg)
-        for row in self.stmt_result:
-            self.fid = row[0]
-            self.capturetime = row[1]
-            self.full_image = row[2]
-            self.manual_labeled = row[3]
-            self.note = row[4]
+        elif mode == 3:
+            dbhdl.execute_stmt(query3, arg)
+        for row in qr.stmt_result:
+            qr.fid = row[0]
+            qr.capturetime = row[1]
+            qr.full_image = row[2]
+            qr.manual_labeled = row[3]
+            qr.note = row[4]
             print("fid = %s,capturetime = %s,manual_labeled = %s,note=%s" % \
-                  (self.fid, self.capturetime, self.manual_labeled, self.note))
+                  (qr.fid, qr.capturetime, qr.manual_labeled, qr.note))
+            self.qrl.append(qr)
 
-    def print_all_fruits(self, arg):
+    # Queries for 'all_fruits'-table, depending on ID
+    def print_all_fruits(self, mode, arg):
+        # The method uses several mode parameters for several queries
         query1 = "SELECT * FROM all_fruits"
         query2 = "SELECT * FROM all_fruits WHERE all_fruits.afid = %s"
-        if arg == "":
-            dbhdl.execute_stmt(query1,"")
-        else:
-            dbhdl.execute_stmt(query2, arg)
-        #On each execution a result is given and saved
-        for row in self.stmt_result:
-            self.afid = row[0]
-            self.fid = row[1]
-            self.half_image = row[2]
-            self.fruitclass = row[3]
-            self.confidence = row[4]
-            self.prediction = row[5]
-            self.note = row[6]
-            print("afid = %s, fid = %s, fruitclass = %s, confidence = %s, prediction=%s, note = %s" % \
-                  (self.afid, self.fid, self.fruitclass, self.confidence, self.prediction, self.note))
+        query3 = "SELECT * FROM all_fruits WHERE all_fruits.prediction >= %s"
 
+        #TODO: Implementing the confidence filter for query 3.
+        if mode == 1:
+            dbhdl.execute_stmt(query1,"")
+        elif mode == 2:
+            dbhdl.execute_stmt(query2, arg)
+        elif mode == 3:
+            dbhdl.execute_stmt(query3, arg)
+        for row in qr.stmt_result:
+            qr.afid = row[0]
+            qr.fid = row[1]
+            qr.half_image = row[2]
+            qr.fruitclass = row[3]
+            qr.confidence = row[4]
+            qr.prediction = row[5]
+            qr.note = row[6]
+
+            print("afid = %s, fid = %s, fruitclass = %s, confidence = %s, prediction=%s, note = %s" % \
+                  (qr.afid, qr.fid, qr.fruitclass, qr.confidence, qr.prediction, qr.note))
+            self.qrl.append(qr)
+
+    # Central 'image'-table containing mainly images with mixed fruit, timestamps, a manual flag and notes
     def insert_fridgelog(self, capturetime, full_image, manual_labeled, note):
         self.cursor.execute("INSERT INTO fridgelog (capturetime, full_image, manual_labeled, note) VALUES (%s, %s, %s, %s)", \
                        (capturetime, full_image, manual_labeled, note))
         self.db.commit()
 
+    # Supplementary table containing a seperate image with highlighted specific fruits, along with the classification data
     def insert_all_fruits(self, fid, half_image, fruit_class, confidence, prediction, note):
         self.cursor.execute(
             "INSERT INTO all_fruits (fid, half_image, class, confidence, prediction, note) VALUES (%s, %s, %s, %s, %s, %s)", \
@@ -137,24 +156,27 @@ if __name__ == "__main__":
     ## Open database connection with Database Handle
     dbhdl = MySQLConnector(host, user, pw, dbc)
     dbhdl.connect()
+    qr = QueryResult() #Single query result object. List of results is qrl
 
     # Should be a list of objects, right now it's only one single object for one row
-    #qr = QueryResult()
+
     #Loading images
-
-    # Working well
-    #ToDo: load bytestream from camera module
     img = read_file('C:\\fatcat.jpg')
-    #dbhdl.insert_fridgelog(datetime.now(), img, 1, "hey")
-    #dbhdl.insert_all_fruits(1, img, "T", 0.25, 0.65, "ha")
+    dbhdl.insert_fridgelog(datetime.now(), img, 1, "hey")
+    dbhdl.insert_all_fruits(1, img, "T", 0.25, 0.65, "ha")
 
-    # Parsing Bug
-    dbhdl.print_fridgelog("1")
-    # print(dbhdl.stmt_result)
+    #Works
+    #dbhdl.print_fridgelog(1,"")
+    #dbhdl.print_fridgelog(2, "1")
+    #dbhdl.print_fridgelog(3, "0.85")
 
-    #Working well
-    #dbhdl.print_fridgelog("")
-    #dbhdl.print_all_fruits("")
+    dbhdl.print_all_fruits(1,"")
+    dbhdl.print_all_fruits(2,"1")
+    dbhdl.print_all_fruits(3,"0.75")
+
+    #Works
+    #dbhdl.cursor.execute("SELECT * FROM fridgelog WHERE fridgelog.fid = %s" % "1")
+    #print(dbhdl.cursor.fetchall())
 
     dbhdl.db.commit()
     dbhdl.disconnect()
