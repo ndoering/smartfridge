ae6c8b9749d5956b8189ddc1f01e86e160f821ba
Chris <31420426+chrzby@users.noreply.github.com>
2018-01-14 00:09:17 +0100

    Initialisierung und Aufruf des SQL Connectors.
    Den Aufruf aus dem Main Skript konnte ich leider nicht testen, da mir das Clarifai Modul versagt. Hier müsste in das initialisierte SQL Connector Objekt in die Insert Methoden die Werte von Clarifai und der Bytestream geladen werden.

--- smartfridge/smartfridge.py
+++ smartfridge/smartfridge.py
@@ -1,19 +1,19 @@
 from camera import camera_handler as ch
 from clarifai_connector import clarifai_connector as cc
+import sql_connector as dbcon
 import configuration_management as conf
 import slack_connector as sc
+from datetime import datetime
 
 
 if __name__ == "__main__":
     # slack stuff
     c = conf.Configuration()
     bot = sc.Slackbot(c)
-    #bot.speak()
-
+    bot.speak()
 
     # take picture (returns bytes)
     streambuffer = ch.take_picture()
-    
 
     # call clarifai API
     print("Start classification.")
@@ -22,3 +22,20 @@ if __name__ == "__main__":
     print("App and Model loaded.")
     ccall = cc.ClarifaiCall(clarifaiApp, model, streambuffer)
     print(ccall.call()) # JSON response
+
+    # Database connection
+    ## Load DB configuration from config.ini within module package
+    c = conf.Configuration()
+    host = c.config["MYSQL"]["Host"]
+    user = c.config["MYSQL"]["User"]
+    pw = c.config["MYSQL"]["Password"]
+    dbc = c.config["MYSQL"]["Database"]
+    ## Open database connection with Database Handle
+    dbhdl = dbcon(host, user, pw, dbc)
+    dbhdl.connect()
+
+    # ToDo: Using the Clarifai, along with the buffered stream data as parameters to save it into the database.
+    # capturetime, full_image, manual_labeled, note
+    dbhdl.insert_fridgelog(datetime.now(), streambuffer, 0, "Pic from streambuffer")
+    # fid, half_image, fruit_class, confidence, prediction, note
+    dbhdl.insert_all_fruits(1, streambuffer, "T", 0.25, 0.65, "ha")
