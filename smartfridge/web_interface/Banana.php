f27a58fdc70965849ea2e943b8544c7564e1ac4d
Chris <31420426+chrzby@users.noreply.github.com>
2018-01-21 15:59:00 +0100

    Implemented Button execution.
    Now the buttons can control php code. For instance, an exemplary "whoami" terminal-command is executed by pressing the picture button. Also SQL Statements and Image display from the DB works. Please feel free to try it on your configuration.
    I suggest to agree on the interface's final look and functionality on Tuesday.
    Cheers.

--- smartfridge/web_interface/Banana.php
+++ smartfridge/web_interface/Banana.php
@@ -3,24 +3,24 @@ require_once 'dbconfig.php';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-       
+    
     // Selecting latest entry from joined tables.
-    $sql_last_entry = 'SELECT af.afid, fl.fid, class, confidence, prediction, MAX(capturetime), full_image 
-                    FROM fridgelog fl, all_fruits af 
+    $sql_last_entry = 'SELECT af.afid, fl.fid, class, confidence, prediction, MAX(capturetime), full_image
+                    FROM fridgelog fl, all_fruits af
                     WHERE af.fid = fl.fid';
     
     $q_le = $pdo->query($sql_last_entry);
     $q_le->setFetchMode(PDO::FETCH_ASSOC);
     
-     while ($row = $q_le->fetch()):
-        $class = $row['class'];
-        $prediction = $row['prediction'];
-        $confidence = $row['confidence'];
-        $capturetime = $row['MAX(capturetime)'];
-        $full_image = $row['full_image'];
-     endwhile;
-
-
+    while ($row = $q_le->fetch()):
+    $class = $row['class'];
+    $prediction = $row['prediction'];
+    $confidence = $row['confidence'];
+    $capturetime = $row['MAX(capturetime)'];
+    $full_image = $row['full_image'];
+    endwhile;
+    
+    
 } catch (PDOException $e) {
     die("Could not connect to the database $dbname :" . $e->getMessage());
 }
@@ -47,15 +47,16 @@ try {
 
 		<div class="col_12" align="center">
 			<h3>Smart Fridge</h3>
-			<!-- Displaying the fruit's latest picture. -->
+			
 			<div class="col_6" style="border: 1px solid red;" align="center">
+			<!-- Displaying the fruit's latest picture. -->
 			<?php
 			echo '<img src="data:image/jpeg;base64,'.base64_encode( $full_image).'" width="550" height="350"/>';
             ?>
 			</div>
 			
-			<!-- Displaying the fruit's edibility information. -->
 			<div class="col_6">
+			<!-- Displaying the fruit's edibility information. -->
 			<?php
 			if ($class == 1)
 			{
@@ -102,22 +103,51 @@ try {
                     </i>
                 </div>';
 			}
-			echo
-			'<div class="notice warning">
-			    <i class="icon-ok icon-large">';
-                    echo "Class: $class, Prediction: $prediction, Confidence: $confidence, Time: $capturetime";
-                echo '</i>
-            </div>';
-			?>
+    			echo
+    			'<div class="notice warning">
+    			    <i class="icon-ok icon-large">';
+                        echo "Class: $class, Prediction: $prediction, Confidence: $confidence, Time: $capturetime";
+                    echo '</i>
+                </div>';?>
 			</div>
+			
+			<div id="btn-example" align="center">
+                <form action="Banana2.php" method="post">
+                    <input type="submit" name="takephoto" value="Take Photo." />
+                    <input type="submit" name="reset" value="Reset." />
+                </form>
+    		</div>
+			
 		</div>
 
 	</div>
 
-	<div id="btn-example" align="center">
-		<button class="large">Take foto</button>
-		<button class="large">Reset</button>
-	</div>
+	<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['takephoto']))
+    {
+        // Todo:
+        // Execute the Command line for taking photo.
+        // Nils could implement a "take snapshot executable."
+        $cmdoutput = exec('whoami');
+        echo         
+        '<div class="notice warning">
+    			    <i class="icon-ok icon-large">
+                        Photo taken. </p>';
+                        echo "Shell Output [whoami]: $cmdoutput";
+                        echo
+                    '</i>
+              </div>';
+    }
+    else if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['reset']))
+    {
+        // Todo:
+        // What should be done on this reset request?
+        echo '<div class="notice warning">
+    			    <i class="icon-ok icon-large">
+                        Reset successfull.
+                    </i>
+              </div>';
+    }?>
 
 </body>
 </html>
